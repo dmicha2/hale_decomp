3d1fb66a70e816e4e4d798ec62eb2299708d47ba
edwibedi <63661953+edwibedi@users.noreply.github.com>
2023-02-15 19:49:21 -0800

    Update hale_decomp.R

--- hale_decomp.R
+++ hale_decomp.R
@@ -1,7 +1,7 @@
 library(data.table)
-setwd("data_file_path")
+setwd("/home/j/Project/Cost_Effectiveness/BEA/BEA_data_2023/HALE")
 # The Utility Funcs
-gen_age_length <- function (dt, terminal_age = 110, terminal_length = 15, process_terminal = T) 
+gen_age_length <- function (dt, terminal_age = 110, terminal_length = 25, process_terminal = T) 
 {
   if (!"age" %in% colnames(dt)) 
     stop("Column age not found")
@@ -35,8 +35,7 @@ mx_ax_to_qx <- function (m, a, t)
   return(result)
 }
 
-
-lx_to_dx <- function(dt, id_vars = NULL, terminal_age = 95, assert_na = T) {
+lx_to_dx <- function(dt, id_vars = NULL, terminal_age = 85, assert_na = T) {
   setorder(dt, "age")
   setorderv(dt, setdiff(id_vars, "age"))
   dt[, dx := lx - shift(lx, 1, type = "lead"), by = id_vars]
@@ -54,7 +53,7 @@ gen_Tx <- function(dt, id_vars) {
   return(dt)
 }
 
-gen_nLx <- function(dt, id_vars = NULL, terminal_age = 95, assert_na = T) {
+gen_nLx <- function(dt, id_vars = NULL, terminal_age = 85, assert_na = T) {
   id_vars <- setdiff(id_vars, "age")
   setorderv(dt, c(id_vars, "age"), order = 1)
   dt[, nLx := age_length * shift(lx, 1, type = "lead") + ax * dx, by = id_vars]
@@ -74,7 +73,6 @@ get_full_life_table <- function(data_table, by_vars = NULL){
   data_table[, qx := mx_ax_to_qx(mx, ax, age_length)]
   data_table[age == max(age), qx := 1]
   
-  
   by_vars <- setdiff(by_vars, c("age_group_id", "age"))
   data_table2 <- data_table[, c(by_vars, "age_group_id", "age", "age_length", "mx", "ax", "qx"), with = FALSE]
   
@@ -84,9 +82,9 @@ get_full_life_table <- function(data_table, by_vars = NULL){
   # lx
   setorderv(data_table2, c(by_vars, "age_group_id", "age"))
   
-  data_table2[age_group_id != 235, lx := cumprod(px)/ px, by = by_vars]
-  data_table2[, term_lx := .SD[age_group_id != 235, prod(px)], by = by_vars]
-  data_table2[age_group_id == 235, lx := term_lx]
+  data_table2[age_group_id != 160, lx := cumprod(px)/ px, by = by_vars]
+  data_table2[, term_lx := .SD[age_group_id != 160, prod(px)], by = by_vars]
+  data_table2[age_group_id == 160, lx := term_lx]
   data_table2[, term_lx := NULL]
   
   # dx
@@ -113,47 +111,63 @@ setnames(data_t, "age_group_years_start", "age")
 gen_age_length(data_t, terminal_age = max(data_t$age))
 
 id_vars <- c("cause_id", "draw", "sex_id", "year", "age_group_id", "age_length", "age")
-dt <- melt(data_t, id.vars = id_vars, measure.vars=c("acmr", "cr_ylds", "crmr", "ylds"))
+
+# Lasanthi suggested to replaced line 115 with line 116.
+# setnames(data_t, c("ylds", "all_cause_ylds"), c("cs_ylds", "ylds"))
+setnames(data_t, "ylds", "cs_ylds")
+
+dt <- melt(data_t, id.vars = id_vars, measure.vars=c("acmr", "cr_ylds", "crmr", "all_cause_ylds"))
 dt[, cause_replaced := as.integer(variable %like% "^cr")]
-dt[, variable := ifelse(variable %like% "mr$", "mx", "ylds")]
+dt[, variable := ifelse(variable %like% "mr$", "mx", "all_cause_ylds")]
 dt <- dcast(dt, as.formula(paste0(paste(c(id_vars, "cause_replaced"), collapse=" + "), " ~ variable")),
             value.var = "value")
 
-dt <- dt[, .(cause_id, draw, sex_id, year, cause_replaced, age_group_id, age, age_length, mx, ylds)]
+dt <- dt[, .(cause_id, draw, sex_id, year, cause_replaced, age_group_id, age, age_length, mx, all_cause_ylds)]
 id_vars <- setdiff(c(id_vars, "cause_replaced"), c("age_group_id", "age_length", "age"))
 
 setorderv(dt, c(id_vars, "age"))
 
 lt <- get_full_life_table(dt, by_vars = id_vars)
+
 lt[, c("ax", "qx", "px", "dx", "Tx", "age_length", "mx") := NULL]
 dt[, c("ax", "mx", "qx") := NULL]
-dt <- merge(lt, dt, by=c(id_vars, "age_group_id"))
+dt <- merge(lt, dt, by=c(id_vars, "age_group_id", "age"))
 
-dt <- merge(dt, data_t[, c(setdiff(id_vars, "cause_replaced"), "age_group_id", "cases"), with=FALSE],
+dt <- merge(dt, data_t[, c(setdiff(id_vars, "cause_replaced"), "age_group_id", "cases", "population"), with=FALSE],
             by=c(setdiff(id_vars, "cause_replaced"), "age_group_id"))
-dt[, haL := nLx * ylds / cases]
-setorder(dt, c(id_vars, "age"), order = 1)
+dt[, haL := nLx * (1 - (all_cause_ylds / population))]
+
+setorderv(dt, c(id_vars, "age"), order = -1)
 dt[, hale := cumsum(haL)/lx, by=id_vars]
+setorderv(dt, c(id_vars, "age"), order = 1)
 
 id_vars <- setdiff(id_vars, "cause_replaced")
 
 dt <- dcast(dt,
             as.formula(paste0(paste(c(id_vars, "age_group_id"), collapse=" + "), " ~ cause_replaced")),
-            value.var=c("ex", "hale", "ylds"))
+            value.var=c("ex", "hale", "all_cause_ylds"))
 
 setnames(dt, names(dt), gsub("_1$", "_cause_replaced", names(dt)))
 setnames(dt, names(dt), gsub("_0$", "", names(dt)))
 
-input_data <- fread("./hale_decomp_input_data.csv")
+input_data <- fread("./hale_decomp_input_data_final.csv")
+
 dt <- merge(dt, unique(input_data[, .(cause_id, gbd_cause_name, dex_cause_id, dex_cause_name)]),
             by="cause_id")
 setcolorder(dt, c("cause_id", "gbd_cause_name", "dex_cause_id", "dex_cause_name",
                   "draw", "sex_id", "year", "age_group_id",
-                  "hale", "hale_cause_replaced", "ex", "ex_cause_replaced", "ylds", "ylds_cause_replaced"))
+                  "hale", "hale_cause_replaced", "ex", "ex_cause_replaced", "all_cause_ylds", "all_cause_ylds_cause_replaced"))
 fwrite(dt, "./cause_replaced_life_table.csv")
-dt <- dt[age_group_id == 235]
+
+dt <- dt[age_group_id == 0]
 dt[, age_group_id := NULL]
 dt[, hale_effect := hale_cause_replaced - hale]
+
 fwrite(dt, "./decomp_results_by_draw.csv")
-dt <- dt[, lapply(.SD, mean), by=.(cause_id, gbd_cause_name, dex_cause_id, dex_cause_name, year),
-         .SDcols=c("hale_effect", "hale", "hale_cause_replaced", "ex", "ex_cause_replaced", "ylds", "ylds_cause_replaced")]
+
+# creating a data frame containing draw 0 only.
+dt_draw_0 <- dt[draw == 0, ]
+fwrite(dt_draw_0, "./draw_0_decomp_results_by_draw.csv")
+
+#dt <- dt[, lapply(.SD, mean), by=.(cause_id, gbd_cause_name, dex_cause_id, dex_cause_name, year),
+#         .SDcols=c("hale_effect", "hale", "hale_cause_replaced", "ex", "ex_cause_replaced", "all_cause_ylds", "ylds_cause_replaced")]
