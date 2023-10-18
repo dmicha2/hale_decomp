35ea7711fef1f0402c1925b236aa27fcc66f859f
edwibedi <63661953+edwibedi@users.noreply.github.com>
2023-10-18 09:23:09 -0700

    Update get_cause_replaced_mort_rate_spending.R
    Updated the cause-replaced mortality rate calculations with non- cause replaced mortality rate (i.e., we keep year specific mortality rates for each of the two individual years) to make results comparable with Cutler et. al.

--- updated_code_06_29_2023/get_cause_replaced_mort_rate_spending.R
+++ updated_code_06_29_2023/get_cause_replaced_mort_rate_spending.R
@@ -19,6 +19,7 @@ dt[cases == 0, case_rate := 0]
 dt[, deaths_per_case := cs_deaths / cases]
 dt[cases == 0, deaths_per_case := 0]
 dt[, ac_mr := ac_deaths / population]
+dt[, cs_mr := cs_deaths / population]
 
 # Create the cause-deleted death rate by subtracting the cause-specific cs_deaths from the total cs_deaths.
 dt[, cause_del_deaths := ac_deaths - cs_deaths]
@@ -32,27 +33,49 @@ cause_replace <- function(d, start_year, end_year){
   # Reshape to wide to create separate columns for separate years.
   d <- dcast(d,
              cause_id + draw + age_group_id + sex_id + location_id + age_group_years_start + age_group_years_end ~ year_id,
-             value.var=c("population", "cause_del_mr", "ac_mr",
+             value.var=c("population", "cause_del_mr", "ac_mr", "cs_mr",
                          "cases", "case_rate", "deaths_per_case",
                          "cs_spending", "cs_spending_per_case", "ac_spending", "cause_del_expenditure"))
   
   ys <- as.character(c(start_year, end_year))
-  # Create cause-replaced mortality rates by taking the cause-deleted mortality rate for a given year and adding
-  # to it the product of the case rate for the same year times the cs_deaths per case for the other year.
-  d[, paste0("cr_ac_mr_", ys[1]) := get(paste0("cause_del_mr_", ys[1])) + get(paste0("case_rate_", ys[1])) * get(paste0("deaths_per_case_", ys[2]))]
+  
+  # # Create cause-replaced mortality rates by taking the cause-deleted mortality rate for a given year and adding
+  # # to it the product of the case rate for the same year times the cs_deaths per case for the other year.
+  # d[, paste0("cr_ac_mr_", ys[1]) := get(paste0("cause_del_mr_", ys[1])) + get(paste0("case_rate_", ys[1])) * get(paste0("deaths_per_case_", ys[2]))]
+  # d[, paste0("cr_ac_mr_", ys[2]) := get(paste0("cause_del_mr_", ys[2])) + get(paste0("case_rate_", ys[2])) * get(paste0("deaths_per_case_", ys[1]))]
+  # 
+  # d[, paste0("cr_ac_spending_", ys[1]) := get(paste0("cause_del_expenditure_", ys[1])) + get(paste0("cases_", ys[1])) * get(paste0("cs_spending_per_case_", ys[2]))]
+  # d[, paste0("cr_ac_spending_", ys[2]) := get(paste0("cause_del_expenditure_", ys[2])) + get(paste0("cases_", ys[2])) * get(paste0("cs_spending_per_case_", ys[1]))]
+  # 
+  # #New addition cr_cs_mr
+  # d[, paste0("cr_cs_mr_", ys[1]) := get(paste0("case_rate_", ys[1])) * get(paste0("deaths_per_case_", ys[2]))]
+  # d[, paste0("cr_cs_mr_", ys[2]) := get(paste0("case_rate_", ys[2])) * get(paste0("deaths_per_case_", ys[1]))]
+
+
+  #Alternate measure where we keep the year-specific mortality fixed (call these the non-cr results)
+  # Alternatively, if we want to keep mortality rate year-specific, i.e, for 1996 measures, we keep 1996 mortality rate (ie, cr_ac_mr = ac_mr and cr_cs_mr=cs_mr)
+  d[, paste0("cr_ac_mr_", ys[1]) := get(paste0("cause_del_mr_", ys[1])) + get(paste0("case_rate_", ys[1])) * get(paste0("deaths_per_case_", ys[1]))]
+  d[, paste0("cr_cs_mr_", ys[1]) := get(paste0("case_rate_", ys[1])) * get(paste0("deaths_per_case_", ys[1]))]
+
+  # Alternatively, if we want to keep mortality rate year-specific, i.e, for 2016 measures, we keep 2016 mortality rate (ie, cr_ac_mr = ac_mr here)
+  d[, paste0("cr_ac_mr_", ys[2]) := get(paste0("cause_del_mr_", ys[2])) + get(paste0("case_rate_", ys[2])) * get(paste0("deaths_per_case_", ys[2]))]
+  d[, paste0("cr_cs_mr_", ys[2]) := get(paste0("case_rate_", ys[2])) * get(paste0("deaths_per_case_", ys[2]))]
+
+  #The cr_ac_spending remains unchanged as we still use other year values for cs_spending_per_case
   d[, paste0("cr_ac_spending_", ys[1]) := get(paste0("cause_del_expenditure_", ys[1])) + get(paste0("cases_", ys[1])) * get(paste0("cs_spending_per_case_", ys[2]))]
-  d[, paste0("cr_ac_mr_", ys[2]) := get(paste0("cause_del_mr_", ys[2])) + get(paste0("case_rate_", ys[2])) * get(paste0("deaths_per_case_", ys[1]))]
   d[, paste0("cr_ac_spending_", ys[2]) := get(paste0("cause_del_expenditure_", ys[2])) + get(paste0("cases_", ys[2])) * get(paste0("cs_spending_per_case_", ys[1]))]
   d[]
   return(d)
 }
+
 # Call the cause-replace function and select columns to keep
 dt <- cause_replace(dt, year1, year2)
 
 id_vars <- c("cause_id", "draw", "sex_id", "location_id", "age_group_id",
              "age_group_years_start", "age_group_years_end")
-measure_vars <- c(outer(c("cr_ac_mr", "cr_ac_spending", "ac_mr", "cs_spending", 
-                          "ac_spending", "cs_spending_per_case", "cases", 
+measure_vars <- c(outer(c("cr_ac_mr", "cr_ac_spending", "ac_mr", "cs_spending", "ac_spending", 
+                          "cr_cs_mr", "cs_mr",
+                          "cs_spending_per_case", "cases", 
                           "case_rate", "population"), c(1996, 2016), paste, sep="_"))
 dt <- melt(dt, id.vars = id_vars, measure.vars=measure_vars)
 dt[, year := as.integer(gsub(".*_", "", variable))]
@@ -61,7 +84,7 @@ dt <- dcast(dt, cause_id + draw + sex_id + location_id + age_group_id + age_grou
             value.var = "value")
 
 
-fwrite(dt, "./spending_cause_replaced_mort_rates_dex_gdp_aggregated_death_agg.csv")
+fwrite(dt, "./non_cr/spending_not_cause_replaced_mort_rates.csv")
 
 dt_draw_0 <- dt[draw == 0, ]
-fwrite(dt_draw_0, "./draw_0_spending_cause_replaced_mort_rates_dex_gdp_aggregated_death_agg.csv")
+fwrite(dt_draw_0, "./non_cr/draw_0_spending_not_cause_replaced_mort_rates.csv")
